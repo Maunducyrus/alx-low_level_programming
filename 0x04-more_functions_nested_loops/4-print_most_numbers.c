aa4f91ed24294259c2b78e143981e9cf2d9098bc
Maunducyrus <maunducyrus123@gmail.com>
2023-02-24 11:33:09 -0800

    heyy

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  * return: void
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++);
+	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
 			_putchar(c);
