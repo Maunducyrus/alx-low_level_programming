7e99f0dc466bc9bc7f86eee90e79e1e536d118f9
Maunducyrus <maunducyrus123@gmail.com>
2023-02-24 10:31:12 -0800

    helloo

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,16 @@
 
 /**
  * _isupper - uppercase letters
- * @c: char to check
+ * @c: character to check
  *
  * Return: 0 or 1
  */
+int _isupper(int c)
 
-int _isupper(int c);
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
+
 }
