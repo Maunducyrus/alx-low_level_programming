14d42a5bf9a23a7bbf5a8af052ff2b4f92811659
Maunducyrus <maunducyrus123@gmail.com>
2023-03-02 06:20:58 -0800

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+        int i;
+        int j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (j < n && src[j] != '\0')
+        {
+                dest[i] = src[j];
+                j++;
+                j++;
+        }
+        dest[i] = '\0';
+        return (dest);
