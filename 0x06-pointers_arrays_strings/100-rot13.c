6c7cfd542366c6d7766e874d9b0909cbe40e635f
Maunducyrus <maunducyrus123@gmail.com>
2023-03-03 05:07:25 -0800

    updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,22 +7,21 @@
  */
 char *rot13(char *s)
 {
-	int i = 0;
+        int i = 0;
 
-	while (s[i] != '\0')
-	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
-		}
-		i++;
-	}
-	return (s);
+        while (s[i] != '\0')
+        {
+                while ((s[i] >= 'a' && s[i] <= 'z') ||
+                                (s[i] >= 'A' && s[i] <= 'Z'))
+                {
+                        if ((s[i] >= 'a' && s[i] <= 'm') ||
+                                        (s[i] >= 'A' && s[i] <= 'M'))
+                                s[i] += 13;
+                        else
+                                s[i] -= 13;
+                        i++;
+                }
+                i++;
+        }
+        return (s);
 }
-
