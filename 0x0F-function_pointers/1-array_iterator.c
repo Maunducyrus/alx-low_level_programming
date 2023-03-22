79f36a199ef7d4aa7f976c977187a12a7cf6727d
Maunducyrus <maunducyrus123@gmail.com>
2023-03-22 05:46:01 -0700

    just inserted

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,9 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ */
+
