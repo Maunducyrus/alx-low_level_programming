2dec75966f05418602ddbf38a2772672cde1614f
Maunducyrus <maunducyrus123@gmail.com>
2023-04-04 06:19:13 -0700

    updated

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum-listint - calculte the sum of all the data in a listint-t list
+ * sum_listint - calculte the sum of all the data in a listint-t list
  * @head: first node in the linked list
  *
  * Return: reuslting sum
