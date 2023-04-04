dac7ecbf41d9c8ada3049b152f14ab949cedb3b7
Maunducyrus <maunducyrus123@gmail.com>
2023-04-04 07:29:49 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
+ * print_listint_safe - prints a linked list safely
  * @head: list of type listint_t to print
  *
  * Return: number of nodes in the list
